d141fc5571058359c182707625b29a666f206adb
XiomaraAlarcon <xio.carballo51@outlook.es>
2019-03-12 22:45:35 -0600

    alarcon12-03-19

--- WService/Models/Clients.cs
+++ WService/Models/Clients.cs
@@ -8,12 +8,10 @@ namespace WService.Models
     public class Clients
     {
         //Farmacia San Nicolas
-        //public static string URLClientA = "http://localhost:50446/";
         public static string URLClientA = "http://farmaciaa20190312062936.azurewebsites.net/";
-        //Farmacia Farmavalue
-        //public static string URLClientB = "http://localhost:56922/";
-        public static string URLClientB = "http://farmaciab20190312071435.azurewebsites.net/";
         //Farmacia Camila
+        public static string URLClientB = "http://farmaciab20190312071435.azurewebsites.net/";
+        //Farmacia Farmavalue
         public static string URLClientC = "http://localhost:57393/";
     }
 }
